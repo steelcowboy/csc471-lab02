c41d3e32dd9c076b5a09c02b8777f9fb671838e0
Jim Heald <james.r.heald@gmail.com>
2018-01-16 20:01:05 -0800

    Basic red triangle, but a bit holy

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,34 @@ using namespace std;
 
 class Triangle {
 public:
+    float area;
+    int vax, vay, vbx, vby, vcx, vcy;
     int t,l,b,r;
     Triangle (int,int,int,int,int,int);
-}
+    float get_area_b (int, int); 
+    float get_area_c (int, int); 
+};
 
-Triangle::Triangle (int vax, int vay, int vbx, int vby, int vcx, int vcy) {
+Triangle::Triangle (int ax, int ay, int bx, int by, int cx, int cy) {
+    vax = ax; vay = ay;
+    vbx = bx; vby = by;
+    vcx = cx; vcy = cy;
+
     r = max(max(vax, vbx), vcx);
     t = max(max(vay, vby), vcy);
     l = min(min(vax, vbx), vcx);
     b = min(min(vay, vby), vcy);
+
+    area = 1/2.0 * ((vbx - vax)*(vcy - vay) - (vcx - vax)*(vby - vay));
+    cout << "Triangle area: " << area << endl;
+}
+
+float Triangle::get_area_b (int px, int py) {
+    return 1/2.0 * ((vax - vcx)*(py - vcy) - (px - vcx)*(vay - vcy));
+}
+
+float Triangle::get_area_c (int px, int py) {
+    return 1/2.0 * ((vbx - vax)*(py - vay) - (px - vax)*(vby - vay));
 }
 
 int main(int argc, char **argv)
@@ -33,15 +52,27 @@ int main(int argc, char **argv)
 	int height = atoi(argv[3]);
 
     // Triangle bounds
-    Triangle t (argv[4], argv[5], argv[6], argv[7], argv[8], argv[9]);
+    Triangle t (atoi(argv[4]), atoi(argv[5]), 
+            atoi(argv[6]), atoi(argv[7]), 
+            atoi(argv[8]), atoi(argv[9]));
 
 	// Create the image. We're using a `shared_ptr`, a C++11 feature.
 	auto image = make_shared<Image>(width, height);
+
+    float a, b, g;
     
     // Calculate the bounding box
     for (int i = t.l; i <= t.r; i++) {
         for (int j = t.b; j <= t.t; j++) {
-            image->setPixel(i, j, 200, 0, 200);
+            b = t.get_area_b(i, j) / t.area;
+            g = t.get_area_c(i, j) / t.area;
+            a = 1 - b - g;
+            //cout << "alpha: " << a << "\nbeta: " << b << "\ngamma: " << g << endl;
+
+            if (a < 0 || b < 0 || g < 0)
+                continue;
+
+            image->setPixel(i, j, 255, 0, 0);
         }
     }
 
